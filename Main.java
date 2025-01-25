3708e2c1cd3a4fdbb0c90f9bd63760d1666fa5d7
Ganesh <seeramdurgaganesh@gmail.com>
2025-01-25 23:00:35 +0530

    Add initial Java files for basic structure and Hello World example

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Scanner sc = new Scanner(System.in);
+
+        System.out.print("What item would you like to buy? : ");
+        String item = sc.nextLine();
+
+        System.out.print("What is the price of the item? : ");
+        double price = sc.nextDouble();
+
+        System.out.print("How many would you like to buy? : ");
+        int quantity = sc.nextInt();
+
+        System.out.println("\nYou have bought " + quantity + " " + item + "/s");
+        System.out.println("The total price is $" + price * quantity);
+
+        sc.close();*/
+
+
+
+        /*Random random = new Random();
+        
+        boolean number1 = random.nextBoolean();
+        boolean number2 = random.nextBoolean();
+        boolean number3 = random.nextBoolean();
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);*/
+
+
+        
+                
+
+    }
+}
