50c4421808d82b80673fb105db32f03a22bea925
Ganesh <seeramdurgaganesh@gmail.com>
2025-01-26 21:57:20 +0530

    Fix Scanner import and implement grading logic based on input value

--- Cipher/_01.java
+++ Cipher/_01.java
@@ -2,6 +2,16 @@ import java.util.*;
 
 class _01 {
     public static void main(String[] args) {
-        SCanner
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+
+        if(a > 0 && a < 100) {
+            if(a >= 90) System.out.println("Grade A"); 
+            else if(a >= 80 && a < 90) System.out.println(" Grade B");
+            else if(a >= 70 && a < 80) System.out.println(" Grade C");
+            else if(a >= 60 && a < 70) System.out.println(" Grade D");
+            else System.out.println("Grade F"); 
+        }
     }   
 }
