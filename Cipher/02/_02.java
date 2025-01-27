591174cadfa26e4922c9822692a683dc80eca680
Ganesh <seeramdurgaganesh@gmail.com>
2025-01-27 23:04:23 +0530

    Implement input handling and conversion in _02.java

--- Cipher/02/_02.java
+++ Cipher/02/_02.java
@@ -2,6 +2,23 @@ import java.util.*;
 
 public class _02 {
     public static void main(String args[]){
-        
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        double b = s.nextDouble();
+        char c = s.next().charAt(0);
+        boolean d = s.nextBoolean();
+
+        System.out.println("Before conversion:");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+        System.out.println("After conversion:");
+        System.out.println((double)a);
+        System.out.println((int)c);
+
+        s.close();
     }
 }
